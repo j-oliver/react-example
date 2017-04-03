a5186b4b0af8958877ff6bbee986168b2773967d
joliver <mail@janrollmann.de>
2017-04-03 22:14:58 +0200

    Add css stylesheets

--- client/index.js
+++ client/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
+import './style/style.css';
+import './style/graph.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
