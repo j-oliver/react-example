f8d9192b368b590aa23a2e9c17d5967eaacf1f3d
joliver <mail@janrollmann.de>
2017-04-11 01:09:07 +0200

    Add module for small helper functions

---
+++ client/module/helper.js
@@ -0,0 +1,20 @@
+module.exports = {
+  getRandomHSLColor: () => {
+    const hue = Math.round(360 * Math.random());
+    const saturation = '100%';
+    const lightness = '70%';
+
+    return `hsl(${hue}, ${saturation}, ${lightness})`;
+  },
+
+  splitArrayIntoChunks: (array, chunksize) => {
+    const chunkedArray = [];
+
+    for (let i = 0; i < array.length; i += chunksize) {
+      chunkedArray.push(array.slice(i, i + chunksize));
+    }
+
+    return chunkedArray;
+  },
+};
+
