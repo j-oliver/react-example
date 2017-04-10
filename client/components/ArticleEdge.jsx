b9cce67d827b99f176737b3dd203da4cb970d816
joliver <mail@janrollmann.de>
2017-04-11 00:58:03 +0200

    Add react proptypes to article edge

--- client/components/ArticleEdge.jsx
+++ client/components/ArticleEdge.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
-export default class ArticleEdge extends React.Component {
-  constructor(props){
-    super(props);
-  }
-
-  render() {
-    return (
-      <line x1={this.props.fromX} y1={this.props.fromY} x2={this.props.toX} y2={this.props.toY}
-            style={{strokeWidth: 2, stroke: 'rgb(0,0,0)'}}
-      />
-    )
-  }
+export default function ArticleEdge({ fromX, fromY, toX, toY }) {
+  return (
+    <line x1={fromX} y1={fromY} x2={toX} y2={toY} style={{ strokeWidth: 2, stroke: 'rgb(0,0,0)' }} />
+  );
 }
+
+ArticleEdge.propTypes = {
+  fromX: React.PropTypes.number.isRequired,
+  fromY: React.PropTypes.number.isRequired,
+  toX: React.PropTypes.number.isRequired,
+  toY: React.PropTypes.number.isRequired,
+};
